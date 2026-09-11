3b01da5c85208058b6cd02f3976a659ba9b71c9a
YeeCheng-SIT <2601742@sit.singaporetech.edu.sg>
2026-09-11 11:06:28 +0800

    trigger overstock alert

--- auditor.py
+++ auditor.py
@@ -5,7 +5,12 @@ while True:
     stock = input("Please enter stock quantity: ")
 
     if stock.isdigit() and int(stock) >= 0:
+        if int(stock) + inv > 500:
+            print("Stock quantity exceeds maximum limit.")
+            break
+
         inv += int(stock)
+    
 
     else:
         print("Invalid input. Please enter a non-negative integer.")
