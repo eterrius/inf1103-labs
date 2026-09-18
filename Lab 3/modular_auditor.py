014d1e62a95c9f41f6bed5597e1de8451dd8f1ab
Tee Yee Cheng <jonestee18@gmail.com>
2026-09-19 00:30:19 +0800

    refactor input validation

--- Lab 3/modular_auditor.py
+++ Lab 3/modular_auditor.py
@@ -1,22 +1,35 @@
 inv = 0
 wrong = 0
 
-while True:
-    stock = input("Please enter stock quantity: ")
+def get_valid_input():
+    result = input("Please enter stock quantity: ")
 
-    if stock.isdigit() and int(stock) >= 0:
+    if result.isdigit() and int(result) >= 0:
+        return int(result)
+    
+    elif result == "quit":
+        return result
+
+    else:
+        print("Invalid input. Please enter a non-negative integer.")
+        return None
+
+while True:
+    stock = get_valid_input()
+
+    if stock is None:
+        wrong += 1
+
+    elif stock != "quit":
         if int(stock) + inv > 500:
             print("Stock quantity exceeds maximum limit.")
             break
 
         inv += int(stock)
-    
+
 
     else:
-        wrong += 1
-        print("Invalid input. Please enter a non-negative integer.")
 
-    if stock == "quit":
         print(f"Total Units Processed: {inv}")
         print(f"Number of Failed/Rejected Entries: {wrong}")
         break
